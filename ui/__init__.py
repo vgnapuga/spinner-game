914f8e8448f9daa4bc0d66790d41407247137533
OneNull <shavrin_ns@mail.ru>
2025-05-18 14:49:22 +0300

    UI __init__.py realization

---
+++ ui/__init__.py
@@ -0,0 +1,12 @@
+from .main_menu import MainMenu
+from .settings import Settings
+from .game_view import GameView
+from .pause_view import PauseView
+
+
+__all__ = [
+    "MainMenu",
+    "Settings",
+    "GameView",
+    "PauseView",
+]
